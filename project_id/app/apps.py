ca1885510e223e37acb8eafe7062e27b49ee8070
Alex Laird <alexdlaird@gmail.com>
2019-01-23 20:19:36 -0800

    pyngrok support improvements, commenting.

--- project_id/app/apps.py
+++ project_id/app/apps.py
@@ -16,22 +16,22 @@ class CommonConfig(AppConfig):
     verbose_name = "App"
 
     def ready(self):
-        if os.environ.get('RUN_MAIN', None) and \
-                settings.DEV_SERVER and settings.USE_NGROK:
-            CommonConfig.init_ngrok()
+        if os.environ.get('RUN_MAIN', None) and settings.DEV_SERVER and settings.USE_NGROK:
+            # pyngrok will only be installed, and should only ever be initialized, in a dev environment
+            from pyngrok import ngrok
 
-    @staticmethod
-    def init_ngrok():
-        from pyngrok import ngrok
+            # Get the dev server port (defaults to 8000 for Django, can be overriden with the
+            # last arg when calling `runserver`)
+            addrport = urlparse('http://{}'.format(sys.argv[-1]))
+            port = addrport.port if addrport.netloc and addrport.port else 8000
 
-        addrport = urlparse('http://{}'.format(sys.argv[-1]))
-        port = addrport.port if addrport.netloc and addrport.port else 8000
-        public_url = ngrok.connect(port)
-        print('ngrok tunnel "{}" -> "http://127.0.0.1:{}/"'.format(public_url, port))
+            # Open a ngrok tunnel to the dev server
+            public_url = ngrok.connect(port).rstrip("/")
+            print('ngrok tunnel "{}" -> "http://127.0.0.1:{}/"'.format(public_url, port))
 
-        settings.PROJECT_HOST = public_url.rstrip("/")
-
-        CommonConfig.init_webhooks(public_url)
+            # Update any base URLs or webhooks to use the public ngrok URL
+            settings.PROJECT_HOST = public_url
+            CommonConfig.init_webhooks(public_url)
 
     @staticmethod
     def init_webhooks(callback_url):
