30cc49a2f652d44d6307748e5c771bf95ec47329
Alex Laird <contact@alexlaird.com>
2024-02-06 09:16:54 -0600

    Bump support to Django 4.x.

--- conf/urls.py
+++ conf/urls.py
@@ -5,23 +5,23 @@ Base URL configuration.
 import sys
 
 from django.conf import settings as config
-from django.conf.urls import include, url
+from django.urls import include, re_path
 from django.views import static
 
 import {%PROJECT_ID_LOWER%}.app.urls
 
 __author__ = "Alex Laird"
 __copyright__ = "Copyright 2018, Helium Edu"
-__version__ = "1.0.2"
+__version__ = "1.3.0"
 
 urlpatterns = [
     # Include app-specific URL files
-    url(r"^", include({%PROJECT_ID_LOWER%}.app.urls)),
+    re_path(r"^", include({%PROJECT_ID_LOWER%}.app.urls)),
 ]
 
 if config.DEBUG or "test" in sys.argv:
     # Ensure media files are shown properly when using a dev server
     urlpatterns += [
-        url(r"^" + config.MEDIA_URL.lstrip("/") + "(?P<path>.*)$", static.serve, {
+        re_path(r"^" + config.MEDIA_URL.lstrip("/") + "(?P<path>.*)$", static.serve, {
             "document_root": config.MEDIA_ROOT})
     ]
--- project_id/app/urls.py
+++ project_id/app/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import re_path
 from rest_framework.documentation import include_docs_urls
 
 from {%PROJECT_ID_LOWER%}.app.admin import admin_site
@@ -7,16 +7,16 @@ from {%PROJECT_ID_LOWER%}.app.views.apis.statusviews import StatusResourceView
 
 __author__ = "Alex Laird"
 __copyright__ = "Copyright 2019, Helium Edu"
-__version__ = "1.1.0"
+__version__ = "1.3.0"
 
 urlpatterns = [
     # URLs for auto-generated resources
-    url("admin/", admin_site.urls, name="admin"),
-    url("docs/", include_docs_urls(title="{%PROJECT_NAME%} API Documentation"), name="docs"),
+    re_path("admin/", admin_site.urls, name="admin"),
+    re_path("docs/", include_docs_urls(title="{%PROJECT_NAME%} API Documentation"), name="docs"),
 
     ##############################
     # Unauthenticated URLs
     ##############################
-    url("status/", StatusResourceView.as_view({"get": "status"}), name="resource_status"),
-    url("info/", InfoResourceView.as_view({"get": "info"}), name="resource_info"),
+    re_path("status/", StatusResourceView.as_view({"get": "status"}), name="resource_status"),
+    re_path("info/", InfoResourceView.as_view({"get": "info"}), name="resource_info"),
 ]
